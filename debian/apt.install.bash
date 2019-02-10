f1df88c79d309e04a19b48a2dd3c57f947d5088f
ivanpierre <me@ivanpierre.world>
2019-02-10 03:54:27 +0100

    adding i3

--- debian/apt.install.bash
+++ debian/apt.install.bash
@@ -108,3 +108,11 @@ snap install vlc
 echo "Install vscode"
 snap install --classic vscode
 
+####
+# i3 (debian repository)
+/usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2019.02.01_all.deb \
+ keyring.deb SHA256:176af52de1a976f103f9809920d80d02411ac5e763f695327de9fa6aff23f416
+sudo dpkg -i ./keyring.deb
+sudo bash -c 'echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list'
+sudo apt update
+sudo apt install i3
